c5732502f36c0d893252dd59106b5c1b33176529
Zach McClure <mcc17067@byui.edu>
2024-05-04 19:17:02 -0600

    personal assignment

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+const app = express();
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
 });
